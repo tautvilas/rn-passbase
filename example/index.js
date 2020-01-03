34b0171a1210e63a7f56db513b48d2fe715aa128
Tautvilas <connect@tautvilas.lt>
2020-01-03 17:43:32 +0200

    update RN version

--- example/index.js
+++ example/index.js
@@ -1,9 +1,67 @@
 /**
  * @format
  */
-
-import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import React from 'react';
+import {Navigation} from 'react-native-navigation';
+import { TouchableOpacity, Text } from 'react-native';
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent(`exampleApp`, () => App);
+Navigation.registerComponent(`root`, () => props => {
+  return (
+    <TouchableOpacity
+      style={{flex: 1, justifyContent: 'center'}}
+      onPress={() => {Navigation.showModal({
+        stack: {
+          children: [{
+            component: {
+              name: 'exampleApp',
+              passProps: {
+                text: 'stack with one child'
+              },
+              options: {
+                topBar: {
+                  title: {
+                    text: 'Modal'
+                  }
+                }
+              }
+            }
+          }]
+        }
+      })}}
+    ><Text>Press me to push auth screen</Text></TouchableOpacity>
+  )
+});
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setDefaultOptions({
+    statusBar: {
+      style: 'light',
+    },
+    layout: { orientation: ['portrait'] },
+    topBar: {
+      background: {
+        color: 'rgb(103, 57, 255)'
+      },
+      title: {
+        color: 'white',
+        fontFamily: 'Roboto-Light',
+      },
+      leftButtonColor: 'white',
+      rightButtonColor: 'white',
+      backButton: {
+        color: 'white',
+      },
+    },
+  });
+
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: "root"
+      }
+    }
+  });
+});
+
